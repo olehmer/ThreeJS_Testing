a729c1302b017b19f2a85e289f992f9354926109
Owen Lehmer <olehmer@gmail.com>
2018-02-11 00:16:20 -0800

    added distant star color shader

--- star/earth_sun_test.js
+++ star/earth_sun_test.js
@@ -1,12 +1,14 @@
 //set the scene size
-var WIDTH = 400;//window.innerWidth;
-var HEIGHT = 400;//window.innerHeight;
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var AU = 1.496*Math.pow(10,8); //1 AU in km
 
 //set the camera attributes
 var VIEW_ANGLE = 45;
 var ASPECT = WIDTH/HEIGHT;
-var NEAR = 0.1;
-var FAR = 10000;
+var NEAR = 100;
+var FAR = 50*AU;
 
 //get the HTML container
 var container = document.querySelector('#container');
@@ -19,8 +21,8 @@ var camera = new THREE.PerspectiveCamera(
     NEAR,
     FAR
 );
-camera.position.z = 1; //orbit controls breaks if camera is at 0,0,0
-camera.position.x = 0;
+camera.position.z = 0; //orbit controls breaks if camera is at 0,0,0
+camera.position.x = 1.1*AU;
 camera.position.y = 0;
 
 var scene = new THREE.Scene();
@@ -35,25 +37,34 @@ var controls = new THREE.OrbitControls( camera , renderer.domElement);
 //controls.enableZoom = true;
 //controls.autoRotate = true;
 
+
+//Create the background star texture
+var bg_texture = new THREE.TextureLoader().load("background.png");
+var bg_geometry = new THREE.SphereGeometry(10*AU,100,100);
+var bg_material = new THREE.MeshBasicMaterial({map: bg_texture});
+var bg_sphere = new THREE.Mesh(bg_geometry, bg_material);
+bg_sphere.scale.x = -1; //invert the texture to the inside, where we are
+scene.add(bg_sphere);
+
 //create the star light
-var star_light = new THREE.PointLight(0xffffff, 1, 10000000);
-star_light.position.set(0,0,-500); 
+var star_light = new THREE.PointLight(0xffffff, 1, AU);
+star_light.position.set(0,0,0); 
 scene.add(star_light);
 
 var ambient = new THREE.AmbientLight(0x101010);
 scene.add(ambient);
 
-var star_pos = new THREE.Vector3(0,0,-500);
-var scale = 0.000007187/2.0; //scale the sun radius to 5
+var star_pos = new THREE.Vector3(0,0,0);
+var scale = 1.0;
 var bg_star = new DistantStar(5788, star_pos, scale, camera, scene);
 scene.add(bg_star.getStarToRender());
 
 
-var planet_geometry = new THREE.SphereGeometry(50,20,20);
-var planet_material = new THREE.MeshLambertMaterial( {color:0xff0f99});
+var planet_geometry = new THREE.SphereGeometry(6400.0,50,50);
+var planet_material = new THREE.MeshLambertMaterial( {color:0x331fff});
 var planet = new THREE.Mesh(planet_geometry, planet_material);
-planet.position.x = 125;
-planet.position.z = -350;
+planet.position.x = AU;
+planet.position.z = 0;
 scene.add(planet);
 
 //for the orbit controls, point at the planet
--- star/star.js
+++ star/star.js
@@ -1,5 +1,13 @@
 /*jshint esversion: 6 */
 
+/* ORL - 2/8/18
+ *
+ * Notes:
+ *
+ * */
+
+
+
 class DistantStar{
 
     constructor(temp, position, scale, camera, scene){
@@ -9,55 +17,116 @@ class DistantStar{
         this.camera = camera;
         this.scene = scene;
 
+        //limit the temperature range
+        if(temp < 800){
+            this.temperature = 800.0;
+        }
+        else if(temp>30000.0){
+            this.temperature = 30000.0;
+        }
+
         //set some constants
         this.DSUN = 1392684.0; //solar radius [km]
         this.TSUN = 5778.0; //solar temp [K]
 
-        this.num_points = 50; //the number of points to raycast to on the star
+        //create the star dimensions based on the temp and scale
+        //TODO
+        this.star_radius = this.starRadiusFromTemp();
+        this.resolution = 100;
+
+        //this is set to true when the textures are all loaded
+        this.ready = false;
+
+
+        this.num_points = 10; //the number of points to raycast to on the star
+        this.glow_size_scale = 1.0; //init to 1
 
         this.group = new THREE.Group();
 
-        //create the star dimensions based on the temp and scale
-        //TODO
-        this.star_radius = this.DSUN;
-        var resolution = 100;
 
-        //create the star billboard, texture by: Andreas Ressl and Georg Hammershmid
-        var texture = new THREE.TextureLoader().load("star_glow.png");
-        var geometry = new THREE.PlaneBufferGeometry(this.star_radius*30,
-            this.star_radius*30);
-        var material = new THREE.MeshBasicMaterial( {map: texture} );
-        material.transparent = true;
-        material.blending = THREE.AdditiveBlending;
-        this.star_billboard = new THREE.Mesh( geometry, material );
-        this.star_billboard.position.copy(this.position);
-        this.star_billboard.renderOrder = Infinity; //render on top of everything
+        //store the color data for the star-temp here
+        this.star_color_data = [];
 
-        this.star_billboard.onBeforeRender = function(renderer, scene,camera, 
-                geometry, material, group){
-            //the line below will render the star glow behind the planet until occulted
-            renderer.clearDepth();
-            //always face the camera
-            this.quaternion.copy( camera.quaternion );
-        };
-        this.updateGlowSize();
+        //IMPORTANT: this function call here kicks off the whole init process
+        this.loadImageTexture();
 
-        var star_sphere_mat = new THREE.MeshBasicMaterial({color:0x000000});
-        star_sphere_mat.blending = THREE.AdditiveBlending;
-        var star_sphere_geo = new THREE.SphereBufferGeometry(
-            this.star_radius*this.scale,resolution,resolution);
-        this.star_sphere = new THREE.Mesh(star_sphere_geo, star_sphere_mat);
-        this.star_sphere.position.copy(this.position); 
-        this.star_sphere.onBeforeRender = function(renderer, scene,camera, 
-                geometry, material, group){
-            //always face the camera
-            this.quaternion.copy( camera.quaternion );
+//        var texture = new THREE.TextureLoader().load("star_glow.png", 
+//            (function(texture){
+//                this.uniforms/texture.value = texture;
+//                this.loadImageTexture();
+//            }).bind(this));
+        
+        this.uniforms = {
+            colorbar:{
+                type:"fv1",
+                value:[]
+            },
+            texture:{
+                type:"t",
+                value: new THREE.TextureLoader().load("star_glow.png")
+            },
+            temperature:{
+                type:"f",
+                value:this.temperature
+            }
         };
 
-        this.initStarOcclusionPoints();
+            
 
-        //add the star sphere and billboard to a group for rendering
-        this.group.add(this.star_sphere, this.star_billboard);
+
+        //the shaders for the star glow
+        this.vShader = 
+            "varying vec2 vUv;\n"+
+            "void main()	{\n"+
+            "vUv = uv;\n"+
+                "gl_Position = projectionMatrix*\n"+
+                              "modelViewMatrix*\n"+
+                              "vec4( position, 1.0 );\n"+
+            "}\n"
+        ;
+
+        this.fShader = 
+            "varying vec2 vUv;\n"+
+            "uniform float colorbar[12];\n"+
+            "uniform sampler2D texture;\n"+
+            "uniform float temperature;\n"+
+            "void main()	{\n"+
+                "vec3 final_c;\n"+
+                "vec4 c_tex = texture2D(texture, vUv);\n"+
+                "float tex_r = c_tex.r; //get the grayscale from r (since all same)\n"+
+                "float magic_sauce = 0.00007*temperature+0.6;\n"+
+                "if(magic_sauce<1.0){\n"+
+                    "magic_sauce = 1.0;\n"+
+                "}\n"+
+                "float color_y = (1.0-tex_r+0.125)*3.0;\n"+
+                "int base = int(floor(color_y));\n"+
+                "float c_fact = color_y-float(base);\n"+
+                "if (base==0){\n"+
+                    "vec3 c1 = vec3(colorbar[0],colorbar[1],colorbar[2]);\n"+
+                    "vec3 c2 = vec3(colorbar[3],colorbar[4],colorbar[5]);\n"+
+                    "final_c = (c1*(1.0-c_fact) + c2*c_fact)*magic_sauce;\n"+
+                "}\n"+
+                "else if (base==1){\n"+
+                    "vec3 c1 = vec3(colorbar[3],colorbar[4],colorbar[5]);\n"+
+                    "vec3 c2 = vec3(colorbar[6],colorbar[7],colorbar[8]);\n"+
+                    "final_c = (c1*(1.0-c_fact) + c2*c_fact)*magic_sauce;\n"+
+                "}\n"+
+                "else if (base==2){\n"+
+                    "vec3 c1 = vec3(colorbar[6],colorbar[7],colorbar[8]);\n"+
+                    "vec3 c2 = vec3(colorbar[9],colorbar[10],colorbar[11]);\n"+
+                    "final_c = (c1*(1.0-c_fact) + c2*c_fact)*magic_sauce;\n"+
+                "}\n"+
+                "else{\n"+
+                    "final_c = vec3(colorbar[9], colorbar[10], colorbar[11])*\n"+
+                        "magic_sauce;\n"+
+                "}\n"+
+                "gl_FragColor = vec4(final_c*c_tex.rgb, 1.0);\n"+
+            "}\n"
+        ;
+
+
+
+
     }
 
 
@@ -73,15 +142,15 @@ class DistantStar{
         var lum = (diam*diam)*Math.pow(this.temperature/this.TSUN,4.0); //luminosity
         var size = 0.016*Math.pow(lum,0.25)/Math.pow(dist,0.5); //size
         
-        this.star_billboard.scale.set(size*this.scale, size*this.scale,1);
+        this.star_billboard.scale.set(size*this.scale*this.glow_size_scale, 
+            size*this.scale*this.glow_size_scale,1);
     }
 
-    updateCamera(camera){
-        this.camera = camera;
-    }
 
     updateStar(objs){
-        this.getStarOccultationFraction(objs);
+        if(this.ready){
+            this.getStarOccultationFraction(objs);
+        }
     }
 
 
@@ -99,7 +168,15 @@ class DistantStar{
             count += this.castRay(dest,origin,objs);
         }
         //TODO this should be done in a shader!
-        this.star_billboard.material.opacity = count/this.num_points;
+        //this.star_billboard.material.opacity = count/this.num_points;
+        this.glow_size_scale = count/this.num_points;
+        if(this.glow_size_scale == 0){
+            this.star_billboard.material.visible = false;
+        }
+        else{
+            this.star_billboard.material.visible = true;
+            this.updateGlowSize();
+        }
     }
 
 
@@ -149,6 +226,195 @@ class DistantStar{
             this.star_sphere.add(point);
         }
     }
+
+    loadImageTexture(){
+        /* Load the star_spectrum.png texture to get the colobar values
+         * we'll use to color the star glow.
+         * */
+        var filepath = "star_spectrum.png";
+
+        var callback = (function(image){
+            var canvas = document.createElement('canvas');
+            canvas.width = image.width;
+            canvas.height = image.height;
+
+            var context = canvas.getContext('2d');
+            context.drawImage(image,0,0);
+
+            var data = context.getImageData(0,0,image.width,image.height);
+            this.initStarWithTextures(data);
+        }).bind(this);
+
+        var loader = new THREE.ImageLoader();
+        loader.load(filepath, callback, undefined, function(){console.error(
+            "Error: couldn't load star temp colorbar image.");});
+    }
+
+
+    initStarWithTextures(data){
+        /* This CB will init the star, it is called after the textures are 
+         * loaded.
+         * */
+        this.ready = true;
+        this.star_color_data = data;
+
+        var x_pos = this.getStarBillboardX();
+
+        this.uniforms.colorbar.value = this.getBillboardArray(x_pos);
+
+        //create the star billboard, texture by: Andreas Ressl and Georg Hammershmid
+        //var texture = new THREE.TextureLoader().load("star_glow.png");
+        var geometry = new THREE.PlaneBufferGeometry(this.star_radius*80,
+            this.star_radius*80);
+        //var material = new THREE.MeshBasicMaterial( {map: texture} );
+        var shaderMaterial = new THREE.ShaderMaterial({
+            vertexShader: this.vShader,
+            fragmentShader: this.fShader,
+            uniforms: this.uniforms
+        });
+        //material.transparent = true;
+        //material.blending = THREE.AdditiveBlending;
+        shaderMaterial.transparent = true;
+        shaderMaterial.blending = THREE.AdditiveBlending;
+        this.star_billboard = new THREE.Mesh( geometry, shaderMaterial);
+        this.star_billboard.position.copy(this.position);
+        this.star_billboard.renderOrder = Infinity; //render on top of everything
+        this.star_billboard.onBeforeRender = function(renderer, scene,camera, 
+                geometry, material, group){
+            //the line below will render the star glow behind the planet until occulted
+            renderer.clearDepth();
+            //always face the camera
+            this.quaternion.copy( camera.quaternion );
+        };
+        this.updateGlowSize();
+
+
+        //create the star sphere (the actual star), just a sphere
+        var color = this.getStarColor(x_pos);
+        var star_color = new THREE.Color(color.r/255.0, color.g/255.0, color.b/255.0);
+        var star_sphere_mat = new THREE.MeshBasicMaterial({color:star_color});
+        star_sphere_mat.blending = THREE.AdditiveBlending;
+        var star_sphere_geo = new THREE.SphereBufferGeometry(
+            this.star_radius*this.scale,this.resolution,this.resolution);
+        this.star_sphere = new THREE.Mesh(star_sphere_geo, star_sphere_mat);
+        this.star_sphere.position.copy(this.position); 
+        this.star_sphere.onBeforeRender = function(renderer, scene,camera, 
+                geometry, material, group){
+            //always face the camera
+            this.quaternion.copy( camera.quaternion );
+        };
+        this.initStarOcclusionPoints();
+        
+        //add the star sphere and billboard to a group for rendering
+        this.group.add(this.star_sphere, this.star_billboard);
+    }
+
+    getStarColor(x){
+        //get the average x colors
+        var pos_fl = Math.floor(x)*4;
+        var pos_cl = Math.ceil(x)*4;
+        var data = this.star_color_data.data;
+
+        var fl_fact = 0;
+        var cl_fact = 0;
+
+        if(pos_fl/4 == x){
+            fl_fact = 0.5;
+            cl_fact = 0.5;
+        }
+        else{
+            fl_fact = x-pos_fl/4;
+            cl_fact = pos_cl/4-x;
+        }
+
+        var shift = this.getTempColorShift();
+
+        return { r: shift[0]*(data[pos_fl]*fl_fact + data[pos_cl]*cl_fact), 
+                 g: shift[1]*(data[pos_fl+1]*fl_fact + data[pos_cl+1]*cl_fact), 
+                 b: shift[2]*(data[pos_fl+2]*fl_fact + data[pos_cl+2]*cl_fact), 
+                 a: data[pos_fl+3]*fl_fact + data[pos_cl+3]*cl_fact
+        };
+    }
+
+    getBillboardArray(x){
+        //get the average x colors
+        var width = 1024;
+        var pos_fl = Math.floor(x)*4;
+        var pos_cl = pos_fl + 4;
+        var data = this.star_color_data.data;
+
+        var fl_fact = 0;
+        var cl_fact = 0;
+
+        if(pos_fl/4 == x){
+            fl_fact = 0.5;
+            cl_fact = 0.5;
+        }
+        else{
+            fl_fact = 1.0-(x-pos_fl/4);
+            cl_fact = 1.0-(pos_cl/4-x);
+        }
+
+        var pos1 = pos_fl;
+        var pos2 = pos_fl+width*4;
+        var pos3 = pos_fl+(width*2)*4;
+        var pos4 = pos_fl+(width*3)*4;
+
+        var result = [data[pos1]*fl_fact/255.0 + data[pos1]*cl_fact/255.0,
+                      data[pos1+1]*fl_fact/255.0 + data[pos1+1]*cl_fact/255.0,
+                      data[pos1+2]*fl_fact/255.0 + data[pos1+2]*cl_fact/255.0,
+                      data[pos2]*fl_fact/255.0 + data[pos2]*cl_fact/255.0,
+                      data[pos2+1]*fl_fact/255.0 + data[pos2+1]*cl_fact/255.0,
+                      data[pos2+2]*fl_fact/255.0 + data[pos2+2]*cl_fact/255.0,
+                      data[pos3]*fl_fact/255.0 + data[pos3]*cl_fact/255.0,
+                      data[pos3+1]*fl_fact/255.0 + data[pos3+1]*cl_fact/255.0,
+                      data[pos3+2]*fl_fact/255.0 + data[pos3+2]*cl_fact/255.0,
+                      data[pos4]*fl_fact/255.0 + data[pos4]*cl_fact/255.0,
+                      data[pos4+1]*fl_fact/255.0 + data[pos4+1]*cl_fact/255.0,
+                      data[pos4+2]*fl_fact/255.0 + data[pos4+2]*cl_fact/255.0];
+        return result;
+    }
+
+
+    getStarBillboardX(){
+        /* Get the X position in the star color spectrum texture to load
+         * the appropriate color.
+         * */
+
+        //IMPORTANT: the spectrum has to be 1024 long!
+        return (this.temperature-800.0)/29200.0*1024.0;
+    }
+
+    
+    getTempColorShift(){
+        var r = this.temperature*(0.0534/255.0)-(43.0/255.0);
+        var g = this.temperature*(0.0628/255.0)-(77.0/255.0);
+        var b = this.temperature*(0.0735/255.0)-(115.0/255.0);
+        return [r,g,b];
+    }
+
+
+    starRadiusFromTemp(){
+        /*
+        Get the estimated radius from the star.
+        Returns the star radius in [km]
+        */
+        var T = this.temperature;
+        var rad = 0;
+        if (T >= 4200.0){ //the upper limit for the Mann (2015) equation
+            rad = 0.000196203*T - 0.134051;
+        }
+        else if( T >= 2729.0){ //the limit where the Mann equation is valid
+            var t_mod = T/3500.0;
+            rad = 10.554-33.7546*t_mod+35.1909*Math.pow(t_mod,2)-
+                11.5928*Math.pow(t_mod,3);
+        }
+        else if( T >= 2300.0 ){
+            rad = 0.0001*T-0.1389;
+        }
+        return rad*this.DSUN/2.0;
+    }
+
 
 
 }
